444f5c23afce72c8d2ea34f0f8eb2733105af369
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-18 14:17:15 +0100

    estrellas por la pantalla

--- estrella.py
+++ estrella.py
@@ -9,7 +9,19 @@ def stars(x1,y1):
             forward(100)
             left (160)
             
-
+'''
+def movement(x2, y2):
+    if (x2, y2) == (90.00, 90.00):
+        x2 = -x2
+        y2 = y2
+    elif (x2, y2) == (-90.00, 90.00):
+        x2 = -x2
+        y2 = -y2
+    elif (x2, y2) == (-90.00, -90.00):
+        x2 = x2
+        y2 = -y2
+    return x2, y2
+'''
 x = 0.00
 y = 0.00
 
@@ -17,9 +29,24 @@ a = 0
 while a != 5:
      
     stars(x,y)
+    #movement(x,y)
+    if (x,y) == (0.00, 0.00):
+        x = 90.00
+        y = 90.00
+    elif (x, y) == (90.00, 90.00):
+        x = -x
+        y = y
+    elif (x, y) == (-90.00, 90.00):
+        x = x
+        y = -y
+    elif (x, y) == (-90.00, -90.00):
+        x = -x
+        y = y
+    
+
+    
     a = a + 1
     
-    y = y + 90.00
-    x = x + 90.00
+    
 
 done()  # finished
