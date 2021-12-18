7c1a4726c660940dff3ba957ead3b3c45017ecc0
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-18 14:00:19 +0100

    más de una estrella

--- estrella.py
+++ estrella.py
@@ -2,23 +2,26 @@ import random
 from turtle import *
 color('blue')  # color of the star 
 
-def stars():
-    while True:
+def stars(x1,y1):
+    goto(x1,y1)
+    for i in range(0,9):
             forward(100)
             left (160)
-            if abs(pos()) < 90: # stop drawing
-                break
-    done() # finished
+            
+            
+           # if abs(pos()) < 1: # stop drawing
+          #      break
+    #done() # finished
 
-x = 90
-y = 90
+x = 0.00
+y = 0.00
 
 a = 0
-while a != 5:
-    
-    penup(h)
-    h = setposition(x,y)
-    
-    stars()
+while a != 50:
+     
+    stars(x,y)
     a = a + 1
-   # x = x + 90
+    y = y + 90.00
+    x = x + 90.00
+
+done()
