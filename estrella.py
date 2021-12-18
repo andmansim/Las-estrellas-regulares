509b79c4383c5cb8fee9ace99c2effc386455b29
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-18 13:42:31 +0100

    intento 1

--- estrella.py
+++ estrella.py
@@ -6,7 +6,19 @@ def stars():
     while True:
             forward(100)
             left (160)
-            if abs(pos()) < 1: # stop drawing
+            if abs(pos()) < 90: # stop drawing
                 break
     done() # finished
-stars()
+
+x = 90
+y = 90
+
+a = 0
+while a != 5:
+    
+    penup(h)
+    h = setposition(x,y)
+    
+    stars()
+    a = a + 1
+   # x = x + 90
