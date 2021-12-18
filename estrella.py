79fa01b6e47b5cf661d5fe6c7737b3ffc9e1550b
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-18 13:08:27 +0100

    comenzar a dibujar

--- estrella.py
+++ estrella.py
@@ -1,2 +1,7 @@
 from turtle import *
-color('green', 'blue')
+color('green', 'blue')  #colores de la estrella
+begin_fill()  # para empezar a dibujar
+
+forward(100)
+end_fill() # para terminar
+done()
