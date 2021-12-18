273ac6023b997c0b1813f34831f790b1dc678382
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-18 19:19:24 +0100

    terminado

--- estrella.py
+++ estrella.py
@@ -18,35 +18,27 @@ def stars(x1,y1):
 # X position
 def movementx():
     x2 = float(random.randint(-200,200))
-   
     if 0 < x2 < 100:
-        x2 = x2 + 110
-      
+        x2 = x2 + 110 
     return x2
 
 # Y position
 def movementy():
     y2 = float(random.randint(-200,200))
-   
     if 0 < y2 < 100:
-        y2 = y2 + 110
-      
+        y2 = y2 + 110 
     return y2
-
 
 x = 0.00
 y = 0.00
 h = [1, 3]
 a = 0
+
+# Painting 5 stars
 while a != 5:
-     
     stars(x,y)
     x = movementx()
     y = movementy()
-  
     a = a + 1
     
-    
-    
-
 done()  # finished
